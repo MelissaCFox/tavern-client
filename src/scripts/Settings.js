a5474b7888852c77232017ae5da6008cf28c661e
MelissaCFox <melissa.caitlin.fox@gmail.com>
2022-01-21 14:45:53 -0600

    changed settings to connect to keroku app url instead of localhost

--- src/scripts/Settings.js
+++ src/scripts/Settings.js
@@ -1,4 +1,5 @@
 export const settings = {
-    apiUrl: "http://localhost:8080"
+    // apiUrl: "http://localhost:8080"
     // apiUrl: "http://flagons.nss.team"
+    apiUrl: "http://tavern-nss.herokuapp.com"
 }
